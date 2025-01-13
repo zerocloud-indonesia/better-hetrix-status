a33e9ce26364aaa40f1aba4038513c1611fed942
Muhammad Azka Attariq <aquaplz12@gmail.com>
2025-01-13 11:19:04 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
-            enableSystem={false}
+            enableSystem={true}
             disableTransitionOnChange
           >
             {children}
